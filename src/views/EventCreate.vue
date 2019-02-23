67a7849dd7712dd81a643df4b4f140b504c9d2d5
Scott Vandehey <fake-scott@spaceninja.com>
2019-02-23 15:11:34 -0800

    05 completing our progress bar

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -9,33 +9,21 @@
       <h3>Name & describe your event</h3>
       <div class="field">
         <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
+        <input v-model="event.title" type="text" placeholder="Add an event title">
       </div>
       <div class="field">
         <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
+        <input v-model="event.description" type="text" placeholder="Add a description">
       </div>
       <h3>Where is your event?</h3>
       <div class="field">
         <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
+        <input v-model="event.location" type="text" placeholder="Add a location">
       </div>
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date" />
+        <datepicker v-model="event.date" placeholder="Select a date"/>
       </div>
       <div class="field">
         <label>Select a time</label>
@@ -43,13 +31,15 @@
           <option v-for="time in times" :key="time">{{ time }}</option>
         </select>
       </div>
-      <input type="submit" class="button -fill-gradient" value="Submit" />
+      <input type="submit" class="button -fill-gradient" value="Submit">
     </form>
   </div>
 </template>
 
 <script>
 import Datepicker from 'vuejs-datepicker'
+import NProgress from 'nprogress'
+
 export default {
   components: {
     Datepicker
@@ -67,6 +57,7 @@ export default {
   },
   methods: {
     createEvent() {
+      NProgress.start()
       this.$store
         .dispatch('event/createEvent', this.event)
         .then(() => {
@@ -76,7 +67,9 @@ export default {
           })
           this.event = this.createFreshEventObject()
         })
-        .catch(() => {})
+        .catch(() => {
+          NProgress.done()
+        })
     },
     createFreshEventObject() {
       const user = this.$store.state.user.user
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,42 +1,62 @@
 <template>
   <div>
     <h1>Events for {{ user.user.name }}</h1>
-    <EventCard v-for="event in event.events" :key="event.id" :event="event"/>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
     <template v-if="page != 1">
-      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">Prev Page</router-link>
-      <template v-if="hasNextPage">&nbsp;|&nbsp;</template>
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+      <template v-if="hasNextPage"
+        >&nbsp;|&nbsp;</template
+      >
     </template>
     <router-link
       v-if="hasNextPage"
       :to="{ name: 'event-list', query: { page: page + 1 } }"
       rel="next"
-    >Next Page</router-link>
+      >Next Page</router-link
+    >
   </div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard.vue'
 import { mapState } from 'vuex'
+import EventCard from '@/components/EventCard.vue'
+import store from '@/store/store'
+
+function getPageEvents(routeTo, next) {
+  const currentPage = parseInt(routeTo.query.page) || 1
+  store
+    .dispatch('event/fetchEvents', {
+      page: currentPage
+    })
+    .then(() => {
+      routeTo.params.page = currentPage
+      next()
+    })
+}
 
 export default {
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
   components: {
     EventCard
   },
-  created() {
-    this.perPage = 3 // Setting perPage here and not in data means it won't be reactive.
-    // We don't need it to be reactive, and this way our component has access to it.
-
-    this.$store.dispatch('event/fetchEvents', {
-      perPage: this.perPage,
-      page: this.page
-    })
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    getPageEvents(routeTo, next)
+  },
+  beforeRouteUpdate(routeTo, routeFrom, next) {
+    getPageEvents(routeTo, next)
   },
   computed: {
-    page() {
-      return parseInt(this.$route.query.page) || 1
-    },
     hasNextPage() {
-      return this.event.eventsTotal > this.page * this.perPage
+      return this.event.eventsTotal > this.page * this.event.perPage
     },
     ...mapState(['event', 'user'])
   }
