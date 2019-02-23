b122a3e75f29747b90802d08f66f950e30a0db28
Scott Vandehey <fake-scott@spaceninja.com>
2019-02-23 09:29:57 -0800

    03 state and getters

--- src/store.js
+++ src/store.js
@@ -1,10 +1,35 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, title: 'Alpha', organizer: 'Alice' },
+      { id: 2, title: 'Beta', organizer: 'Bob' },
+      { id: 3, title: 'Gamma', organizer: 'Charlie' },
+      { id: 4, title: 'Delta', organizer: 'Doug' }
+    ]
+  },
   mutations: {},
-  actions: {}
-});
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
+})
