e0ec61a12ccc527f691d97263b03e63efe2206c8
Scott Vandehey <fake-scott@spaceninja.com>
2019-02-23 17:41:59 -0800

    07 reusable form components - baseinput

--- src/main.js
+++ src/main.js
@@ -2,12 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
-import BaseIcon from '@/components/BaseIcon.vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 import 'nprogress/nprogress.css'
 
-Vue.component('BaseIcon', BaseIcon)
-
 Vue.config.productionTip = false
+
+// Automatically register Base[*].vue components
+// 1. the relative path of the directory to search
+// 2. subdirectories will not be searched
+// 3. regular expression that searches for components starting with "Base"
+//    and ending in .vue or .js
+// 4. removes what's before and after the filename itself
+const requireComponent = require.context(
+  './components', // 1
+  false, // 2
+  /Base[A-Z]\w+\.(vue|js)$/ // 3
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+  const componentName = upperFirst(
+    camelCase(
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1') // 4
+    )
+  )
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 new Vue({
   router,
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -7,23 +7,32 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title">
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description">
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Add an event title"
+        label="Title"
+        v-model="event.title"
+        class="field"
+      />
+      <BaseInput
+        type="text"
+        placeholder="Add a description"
+        label="Description"
+        v-model="event.description"
+        class="field"
+      />
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location">
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Add a location"
+        label="Location"
+        v-model="event.location"
+        class="field"
+      />
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date"/>
+        <datepicker v-model="event.date" placeholder="Select a date" />
       </div>
       <div class="field">
         <label>Select a time</label>
@@ -31,7 +40,7 @@
           <option v-for="time in times" :key="time">{{ time }}</option>
         </select>
       </div>
-      <input type="submit" class="button -fill-gradient" value="Submit">
+      <input type="submit" class="button -fill-gradient" value="Submit" />
     </form>
   </div>
 </template>
