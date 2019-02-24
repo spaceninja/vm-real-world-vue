d7f0c7daf746aad225eac5550b560b3b6c413cc6
Scott Vandehey <fake-scott@spaceninja.com>
2019-02-23 22:56:36 -0800

    12 mixins

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -3,13 +3,14 @@
     <label v-if="label">{{ label }}</label>
     <select
       :value="value"
-      @input="updateValue"
+      @change="updateValue"
       v-bind="$attrs"
       v-on="$listeners"
     >
       <option
         v-for="option in options"
-        :key="option"
+        :value="option"
+        :key="option.id"
         :selected="option === value"
         >{{ option }}</option
       >
@@ -18,25 +19,14 @@
 </template>
 
 <script>
+import { formFieldMixin } from '../mixins/formFieldMixin'
 export default {
-  inheritAttrs: false,
+  mixins: [formFieldMixin],
   props: {
-    label: {
-      type: String,
-      default: ''
-    },
     options: {
       type: Array,
       required: true
-    },
-    value: [String, Number]
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value)
     }
   }
 }
 </script>
-
-<style scoped></style>
