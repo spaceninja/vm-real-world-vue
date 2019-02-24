9acb571f91a45e85120c234f4a7b0cfb59d446d5
Scott Vandehey <fake-scott@spaceninja.com>
2019-02-23 23:12:37 -0800

    minor bugfix

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -16,8 +16,8 @@ export const mutations = {
   SET_EVENTS(state, events) {
     state.events = events
   },
-  SET_EVENTS_TOTAL(state, total) {
-    state.eventsTotal = total
+  SET_EVENTS_TOTAL(state, eventsTotal) {
+    state.eventsTotal = eventsTotal
   },
   SET_EVENT(state, event) {
     state.event = event
@@ -29,6 +29,7 @@ export const actions = {
     return EventService.postEvent(event)
       .then(() => {
         commit('ADD_EVENT', event)
+        commit('SET_EVENT', event)
         const notification = {
           type: 'success',
           message: 'Your event has been created!'
@@ -47,9 +48,8 @@ export const actions = {
   fetchEvents({ commit, dispatch, state }, { page }) {
     return EventService.getEvents(state.perPage, page)
       .then(response => {
-        console.log('Total events are ' + response.headers['x-total-count'])
+        commit('SET_EVENTS_TOTAL', parseInt(response.headers['x-total-count']))
         commit('SET_EVENTS', response.data)
-        commit('SET_EVENTS_TOTAL', response.headers['x-total-count'])
       })
       .catch(error => {
         const notification = {
@@ -63,7 +63,9 @@ export const actions = {
     if (id == state.event.id) {
       return state.event
     }
+
     var event = getters.getEventById(id)
+
     if (event) {
       commit('SET_EVENT', event)
       return event
@@ -75,7 +77,6 @@ export const actions = {
     }
   }
 }
-
 export const getters = {
   getEventById: state => id => {
     return state.events.find(event => event.id === id)
